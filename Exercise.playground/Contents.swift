37f990124357081975ad29648d04a3aefd3e1efd
AndreRodriguezSabino <andresabinocalvo@gmail.com>
2023-09-12 20:21:11 +0100

    For and Functions exercises on Swift

---
+++ Exercise.playground/Contents.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5){
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5){
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
+
+func timeTable(number: Int) {
+    print("This is the Time Table of: \(number)")
+    for i in 1...10 {
+        print(" \(number) X \(i)= \(i * number)")
+    }
+}
+timeTable(number: 7)
+
+func rollDice() -> Int {
+    return Int.random(in: 1...6)
+}
+let result = rollDice()
+print(result)
+
+func personaliseTimeTable(num: Int, end: Int) {
+    print ("Personalise Time Table of: \(num)")
+    for i in 1...end {
+        print(" \(num) X \(i)= \(i * num)")
+    }
+}
+personaliseTimeTable(num: 4, end: 5)
